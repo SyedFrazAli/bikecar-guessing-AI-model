5b97a00cf62d9a778ea43834086047f85a6332e4
SyedFrazAli <21017119-001@uog.edu.pk>
2024-10-22 12:56:52 +0500

    Update web2.py

--- web2.py
+++ web2.py
@@ -3,37 +3,27 @@ from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
 
-# Load the model
 model = load_model('model.keras')
 
 def predict_image(image):
-    # Preprocess the image
+
     image = cv2.resize(image, (64, 64))
     image = image / 255.
     image = np.expand_dims(image, axis=0)
-
-    # Make the prediction
     prediction = model.predict(image)
     
     return 'Car' if prediction < 0.5 else 'Bike'
 
 st.title('Car or Bike Image guessing AI model')
 st.write("Developed by Syed M Fraz Ali")
-st.write("“Warning: This AI has a one-track mind! It’s either ‘car’ or ‘bike’ . Any other photo will cause a serious case of identity crisis!”")
-# Upload the image file
+st.write("“Warning: This AI has a one-track mind! It’s either ‘Car’ or ‘Bike’. Any other photo will cause a serious case of identity crisis!”")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png", "jpeg"])
 
 if file is not None:
-    # Convert the file to an opencv image
+
     file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
 
-    # Display the image
     st.image(image, use_column_width=True)
-
-    # Make a prediction
     prediction = predict_image(image)
-
-    # Show the prediction
     st.write("This image is most likely a ", prediction)
-
